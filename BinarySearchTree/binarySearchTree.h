3e56e84aae256ce6522478d840d3c43c999d2a4b
zjx <zjx@163.com>
2023-12-25 15:13:48 +0800

    [新增]新增树的结点和初始化和插入的API接口

---
+++ BinarySearchTree/binarySearchTree.h
@@ -0,0 +1,28 @@
+#ifndef __BINARY_SEARCH_TREE_H_
+#define __BINARY_SEARCH_TREE_H_
+
+#define ELEMENTTYPE int
+typedef struct BSTreeNode
+{
+    ELEMENTTYPE data;
+    struct BSTreeNode *left;     /* 左子树 */
+    struct BSTreeNode *right;    /* 右子树 */
+    struct BSTreeNode *parent;   /* 父结点 */
+} BSTreeNode;
+
+typedef struct BinarySearchTree
+{
+    /* 根结点 */
+    BSTreeNode * root;
+    /* 树的结点个数 */
+    int size;
+
+} BinarySearchTree;
+
+/* 二叉搜索树的初始化 */
+int binarySearchTreeInit(BinarySearchTree **pBstree);
+
+/* 二叉搜索树的插入 */
+int binarySearchTreeInsert(BinarySearchTree *pBstree, ELEMENTTYPE val);
+
+#endif
