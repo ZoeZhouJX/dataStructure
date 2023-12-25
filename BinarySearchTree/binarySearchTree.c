39a73b0c6d9dd8e947df6642829818256595a8c6
zjx <zjx@163.com>
2023-12-25 16:16:37 +0800

    [修改]完成二叉搜索树的插入结点的API接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* 状态码 */
 enum STATUS_CODE
 {
     ON_SUCCESS,
@@ -10,12 +11,11 @@ enum STATUS_CODE
     INVALID_ACCESS,
 };
 
-
 /* 二叉搜索树的初始化 */
 int binarySearchTreeInit(BinarySearchTree **pBstree)
 {
     int ret = 0;
-    BinarySearchTree * bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
+    BinarySearchTree *bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
     if (bstree == NULL)
     {
         return MALLOC_ERROR;
@@ -24,13 +24,13 @@ int binarySearchTreeInit(BinarySearchTree **pBstree)
     memset(bstree, 0, sizeof(BinarySearchTree) * 1);
     /* 初始化树 */
     {
-    bstree->root = NULL;
-    bstree->size = 0;
+        bstree->root = NULL;
+        bstree->size = 0;
     }
 
-    #if 1
+#if 1
     /* 分配根结点 */
-    bstree->root = (BSTreeNode *)(sizeof(BSTreeNode) * 1);
+    bstree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
     if (bstree->root == NULL)
     {
         return MALLOC_ERROR;
@@ -44,12 +44,30 @@ int binarySearchTreeInit(BinarySearchTree **pBstree)
         bstree->root->right = NULL;
         bstree->root->parent = NULL;
     }
-    #endif
+#endif
 
-    /* 解引用 */
     *pBstree = bstree;
-
     return ret;
+}
+
+static int compareFunc(ELEMENTTYPE val1, ELEMENTTYPE val2)
+{
+#if 0
+    if (val1 < val2)
+    {
+        return -1;
+    }
+    else if (val1 > val2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+#else
+    return val1 - val2;
+#endif
 }
 
 /* 二叉搜索树的插入 */
@@ -68,39 +86,62 @@ int binarySearchTreeInsert(BinarySearchTree *pBstree, ELEMENTTYPE val)
     }
 
     /* travelNode 指向根结点 */
-    BSTreeNode * travelNode = pBstree->root;
-    BSTreeNode * parentNode = pBstree->root;
+    BSTreeNode *travelNode = pBstree->root;
+    BSTreeNode *parentNode = pBstree->root;
 
-    /* 确定符号：到底放在左边还是右边 */
+    /* 确定符号: 到底放在左边还是右边 */
     int cmp = 0;
     while (travelNode != NULL)
     {
         /* 标记父结点 */
         parentNode = travelNode;
-        cmp = val - travelNode->data;
+        cmp = compareFunc(val, travelNode->data);
         /* 插入元素 < 遍历到的结点 */
         if (cmp < 0)
         {
             travelNode = travelNode->left;
         }
-        else if (cmp > 0)     /* 插入元素 > 遍历到的结点 */
+        else if (cmp > 0) /* 插入元素 > 遍历到的结点 */
         {
             travelNode = travelNode->right;
         }
         else
         {
-            /* 插入元素 = 遍历到的结点 */  
-            return ret;        
+            /* 插入元素 = 遍历到的结点 */
+            return ret;
         }
     }
 
+    /* 分配根结点 */
+    BSTreeNode *newBstNode = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if (newBstNode == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(newBstNode, 0, sizeof(BSTreeNode) * 1);
+    {
+        newBstNode->data = 0;
+        newBstNode->left = NULL;
+        newBstNode->right = NULL;
+        newBstNode->parent = NULL;
+    }
+    /* 新结点赋值 */
+    newBstNode->data = val;
+
+    /* 挂在左子树 */
     if (cmp < 0)
     {
-        parentNode->left = (val的结点);
+        parentNode->left = newBstNode;
     }
     else
     {
-        parentNode->right = (val的结点);
+        /* 挂在右子树 */
+        parentNode->right = newBstNode;
     }
+    /* 新结点的parent指针赋值 */
+    newBstNode->parent = parentNode;
+
+    /* 更新树的结点 */
+    (pBstree->size)++;
     return ret;
 }
